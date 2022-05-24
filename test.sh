373b61f8bc187b1df1fbecedacf20a2fe6cc2963
John Grime <jgrime@ou.edu>
2022-05-23 20:28:10 -0500

    First commit

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#URL="https://api.zotero.org/users/1234567/items"
+#API_KEY="***REMOVED***"
+
+# Claire's statistics helper library, and API key generated from my account.
+URL="https://api.zotero.org/groups/2547147/items"
+API_KEY="PUT_YOUR_ACCESS_KEY_HERE"
+
+curl -H "Zotero-API-Key: $API_KEY" $URL > items.json
+
